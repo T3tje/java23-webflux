4aee3bfc9c55c793111721e38a4e492a89c29da7
T3tje <tilman2013@gmail.com>
2023-11-27 15:16:29 +0100

    initial commit

---
+++ src/main/java/de/neuefische/java23webflux/Java23WebfluxApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.java23webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java23WebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java23WebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/java23webflux/Java23WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.java23webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java23WebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
